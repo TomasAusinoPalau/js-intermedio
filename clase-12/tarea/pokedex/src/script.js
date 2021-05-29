bc793ac3d3d34f4cf02f3daad72c2d543b324cbe
Tomas Ausino Palau <tausinopalau@gmail.com>
2021-05-29 00:07:48 -0300

    descripción tarea pokemon clase 12

--- clase-12/tarea/pokedex/src/script.js
+++ clase-12/tarea/pokedex/src/script.js
@@ -11,19 +11,48 @@ for(let i = 1; i <= POKEMON_API_1; i+= evitarEvoluciones) {
     .then(response => response.json())
     .then(responseJSON => {
 
-
-        $(".main-container").append($(`<div id="${responseJSON.name}-container"class="pokemon-container "><p class="name-pokemon">${responseJSON.name}</p></div>`));
-        $(`#${responseJSON.name}-container`).append($(`<div class="${responseJSON.name} img-container"><img class="${responseJSON.name} img-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png"></div>`));
-        $(`#${responseJSON.name}-container`).append($(`<div class="${responseJSON.name} btn-container"><button id="btn-pokemon" class="${responseJSON.name} btn-pokemon">Ver Detalle</button></div>`))
-        
+        addPokemon(responseJSON, i, pokemonContainerCounter)        
         dataPokemones.push(responseJSON)
         pokemonContainerCounter += 1;
+        
+        
     })
     .catch(error => console.error(error))
 }
 
-$(".btn-pokemon").click(mostrarDetalles);
+
 function mostrarDetalles (event) {
-    console.log(event.target)
+    $(".pokemon-stats").removeClass("oculto")
+    let pokemonSeleccionado = event.target.classList[0]
+    let imgPokemonSeleccionado = document.querySelector(`.${pokemonSeleccionado} .img-pokemon`);
+    for (let i = 0; i < dataPokemones.length; i++) {
+        if(dataPokemones[i].name === pokemonSeleccionado) {
+            $(".mini-img-pokemon").append(imgPokemonSeleccionado.cloneNode(true))
+            $(".stats-list .name").text(function() {
+                console.log(dataPokemones[i])
+                return `Nombre: ${dataPokemones[i].name}`
+            })
+            $(".stats-list .type").text(function() {
+                return `Tipo: ${dataPokemones[i].types[0].type.name}`
+            })
+            $(".stats-list .tall").text(function() {
+                return `Altura: ${dataPokemones[i].height * 10} cm`
+            })
+            $(".stats-list .weight").text(function() {
+                return `Altura: ${dataPokemones[i].weight} km`
+            })
+
+        }
+    }
 }
 
+function addPokemon(pokemon, indexPokemon, indexContainerPokemon) {
+        $(".main-container").append($(`<div id="${pokemon.name}-container"class="pokemon-container ${indexContainerPokemon}"><p class="name-pokemon">${pokemon.name}</p></div>`));
+        $(`#${pokemon.name}-container`).append($(`<div class="${pokemon.name} img-container"><img class="${pokemon.name} img-pokemon" id="pokemon-${indexPokemon}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${indexPokemon}.png"></div>`));
+        $(`#${pokemon.name}-container`).append($(`<div class="${pokemon.name} btn-container"><button id="btn-pokemon" class="${pokemon.name} btn-pokemon">Ver Detalle</button></div>`))
+        
+}
+
+setTimeout(() => {
+    $(".btn-pokemon").click(mostrarDetalles);
+}, 3000)
